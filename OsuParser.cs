394ad152a12bcaee8a83419e915af015a7135f22
Bien Pham <pham.bien10@gmail.com>
2018-02-22 20:24:28 +0700

    First version

---
+++ OsuParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Exceptions;
+using IniParser.Model;
+using IniParser.Parser;
+
+namespace OsuCleaner
+{
+    public class OsuParser : IniDataParser
+    {
+        private int _dataCounter = 0;
+
+        protected override void ProcessLine(string currentLine, IniData currentIniData)
+        {
+            currentLine = currentLine.Trim();
+
+            if (LineContainsAComment(currentLine))
+                currentLine = ExtractComment(currentLine);
+
+            if (currentLine == String.Empty)
+                return;
+
+            if (LineMatchesASection(currentLine))
+            {
+                string sectionName = Configuration.SectionRegex.Match(currentLine).Value.Trim();
+                sectionName = sectionName.Substring(1, sectionName.Length - 2).Trim();
+
+                if (sectionName != string.Empty)
+                    _dataCounter = 0;
+
+                ProcessSection(currentLine, currentIniData);
+                return;
+            }
+
+            if (!LineMatchesAKeyValuePair(currentLine))
+            {
+                currentLine = _dataCounter.ToString() + currentIniData.Configuration.KeyValueAssigmentChar.ToString() + currentLine;
+                ++_dataCounter;
+            }
+
+            ProcessKeyValuePair(currentLine, currentIniData);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
 
 namespace OsuCleaner
 {
@@ -10,6 +13,64 @@ namespace OsuCleaner
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var beatmapFolders = Directory.GetDirectories(".\\Songs");
+                
+                foreach (var beatmapFolder in beatmapFolders)
+                {
+                    Console.WriteLine("Parsing " + beatmapFolder);
+                    var osuFiles = Directory.GetFiles(beatmapFolder, "*.osu");
+                    var ignoredFiles = new HashSet<string>();
+
+                    foreach (var osu in osuFiles)
+                    {
+                        var parser = new FileIniDataParser(new OsuParser());
+                        parser.Parser.Configuration.CommentString = "//";
+                        parser.Parser.Configuration.KeyValueAssigmentChar = ':';
+                        parser.Parser.Configuration.SkipInvalidLines = true;
+                        var data = parser.ReadFile(osu);
+                        ignoredFiles.Add(osu);
+                        ignoredFiles.Add(Path.Combine(beatmapFolder, data["General"]["AudioFilename"]));
+
+                        try
+                        {
+                            var ev = data["Events"]["0"].Split(',');
+
+                            if (ev[0] == "Video")
+                                ev = data["Events"]["1"].Split(',');
+
+                            ignoredFiles.Add(Path.Combine(beatmapFolder, ev[2].Substring(1, ev[2].Length - 2)));
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+
+                    var files = Directory.GetFiles(beatmapFolder, "*", SearchOption.AllDirectories);
+
+                    foreach (var file in files)
+                    {
+                        try
+                        {
+                            if (!ignoredFiles.Contains(file))
+                                File.Delete(file);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Warning: Cannot delete " + file);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
